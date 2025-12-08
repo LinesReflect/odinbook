21e3d43e960025f327369325d861ba7a9070c695
Lines Reflect <107893733+LinesReflect@users.noreply.github.com>
2025-12-08 17:53:34 -0600

    Make the cable initializer run only on server starts, not migrations

--- config/initializers/solid_cable.rb
+++ config/initializers/solid_cable.rb
@@ -1,3 +1,6 @@
-Rails.application.config.after_initialize do
-  SolidCable::Record.connects_to database: { writing: :primary }
+# config/initializers/solid_cable.rb
+unless Rails.env.test? || File.split($0).last([ "rake", "rails" ])
+  Rails.application.config.after_initialize do
+    SolidCable::Record.connects_to database: { writing: :primary }
+  end
 end
