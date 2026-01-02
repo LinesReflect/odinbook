4e08f523ef9289fb1becbfad1559337df52db733
Lines Reflect <107893733+LinesReflect@users.noreply.github.com>
2026-01-02 15:56:08 -0600

    Force OmniAuth host for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,12 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  Rails.application.routes.default_url_options[:host] = "mysite-ce1j.onrender.com"
+  Rails.application.routes.default_url_options[:protocol] = "https"
+
+  config.action_controller.default_url_options = {
+    host: "mysite-ce1j.onrender.com",
+    protocol: "https"
+  }
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,6 +9,8 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  OmniAuth.config.full_host = "https://mysite-ce1j.onrender.com"
+
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
