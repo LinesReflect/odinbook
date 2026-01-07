397b5bda931d27a79baf7f0bd20e5a9935728085
Lines Reflect <107893733+LinesReflect@users.noreply.github.com>
2026-01-07 15:52:39 -0600

    Update SendGrid SMTP settings in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,5 +97,14 @@ Rails.application.configure do
   }
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.sendgrid.net",
+    port: 587,
+    domain: "mysite-ce1j.onrender.com",
+    authentication: :plain,
+    user_name: Rails.application.credentials.dig(:sendgrid, :username),
+    password: Rails.application.credentials.dig(:sendgrid, :password),
+    enable_starttls_auto: true
+  }
 end
