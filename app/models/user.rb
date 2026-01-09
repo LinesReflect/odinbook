53480390e6cced4d64410f84ceb6fc56105a1640
Lines Reflect <107893733+LinesReflect@users.noreply.github.com>
2026-01-08 18:47:03 -0600

    Change welcome email to send after commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [ :google_oauth2, :twitter2 ]
 
-  after_create :set_avatar, :send_welcome_email
+  after_commit :send_welcome_email, on: :create
+
+  after_create :set_avatar
 
   validates :email, uniqueness: true
   validates :username, presence: true, uniqueness: true
